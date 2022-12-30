31b87c5e494a25b5dde76c203e1386c088d98430
Shashi <shashikant.shukla10@gmail.com>
2022-12-30 17:50:27 +0530

    End of Season 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
 using System.Text;
 using API.Data;
+using API.Errors;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +21,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200"));
 if (app.Environment.IsDevelopment())
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,8 +21,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accoutService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
+      next: () => this.router.navigateByUrl('/members')
     })
   }
 
